44587b861fe0636420b8237561ceae8c6fb99a2a
sumit-0307 <sumiitt0307@gmail.com>
2025-06-07 05:22:14 +0530

    Updated socket and index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,42 @@
 import express from "express";
-import dotenv from "dotenv"
-import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app,server } from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
 
-dotenv.config()
+dotenv.config();
 
-
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-})
+    credentials: true,
+  })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-    console.log("Server is running on PORT:"+ PORT);
-    connectDB()
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -16,16 +16,16 @@ export function getReceiverSocketId(userId) {
 }
 
 // used to store online users
-    const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {}; // {userId: socketId}
 
-    io.on("connection", (socket) => {
+io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-     // io.emit() is used to send events to all the connected clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
@@ -33,9 +33,5 @@ export function getReceiverSocketId(userId) {
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
-
-
-
-
 
 export { io, app, server };
