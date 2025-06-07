b600c29d1dd844623dd8acc1c3011377bd792b10
sumit-0307 <sumiitt0307@gmail.com>
2025-06-07 05:38:22 +0530

    updated many files

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,29 +1,28 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-{
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    fullName:{
-        type: String,
-        required: true,
+    fullName: {
+      type: String,
+      required: true,
     },
-    password:{
-        type: String,
-        required: true,
-        minlength: 6,
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    profilePic:{
-        type: String,
-        default: "",
+    profilePic: {
+      type: String,
+      default: "",
     },
-},
-    { timestamps: true }
+  },
+  { timestamps: true }
 );
-
 
 const User = mongoose.model("User", userSchema);
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js"
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
